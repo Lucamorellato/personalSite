616eb6875d7071e827878100b861fd5507dec383
lucamorellato <luca.morellato@gmail.com>
2018-12-19 10:56:16 -0500

    Cleaned up .js file and edited some copy in index.html

--- script/script.js
+++ script/script.js
@@ -1,69 +1,49 @@
 const app = {}
 
+
+//nav scroller funciton =---> checks height of body vs where user has scrolled to and assigns classes
+
 app.navScroller = function () {
-   let baseHeight = $("body").height();
+   let initialHeight = $("body").height();
 
    $(window).on('scroll', function () {
-      let scrollPercent = ($(window).scrollTop() / baseHeight);
+      let scrollPercent = ($(window).scrollTop() / initialHeight);
       if (scrollPercent > -1 && scrollPercent < .10) {
          $('.dot').removeClass('dotSelected');
-         $('.dotText').removeClass('dotTextShow')
 
-         // $('.skillsDot').removeClass('dotSelected');
-         // $('.aboutDot').removeClass('dotSelected');
          $('.heroDot').addClass('dotSelected');
-         $('.dotText').addClass('dotTextShow')
-
       }
       else if (scrollPercent > .10 && scrollPercent < .22) {
          $('.dot').removeClass('dotSelected');
 
-         // $('.aboutDot').removeClass('dotSelected');
-         // $('.heroDot').removeClass('dotSelected');
-         // $('.skillsDot').removeClass('dotSelected');
          $('.aboutDot').addClass('dotSelected');
       }
       else if (scrollPercent > .22 && scrollPercent < .35) {
          $('.dot').removeClass('dotSelected');
 
-         // $('.aboutDot').removeClass('dotSelected');
-         // $('.projectsDot').removeClass('dotSelected');
          $('.section1Dot').addClass('dotSelected');
-         // $('.heroDot').removeClass('dotSelected');
       }
       else if (scrollPercent > .35 && scrollPercent < .49) {
          $('.dot').removeClass('dotSelected');
 
-         // $('.skillsDot').removeClass('dotSelected');
-         // $('.contactDot').removeClass('dotSelected');
          $('.section2Dot').addClass('dotSelected');
-         // $('.heroDot').removeClass('dotSelected');
       }
       else if (scrollPercent > .49 && scrollPercent < .64) {
          $('.dot').removeClass('dotSelected');
 
-         // $('.aboutDot').removeClass('dotSelected');
-         // $('.projectsDot').removeClass('dotSelected');
          $('.section3Dot').addClass('dotSelected');
-         // $('.heroDot').removeClass('dotSelected');
       }
       else if (scrollPercent > .64 && scrollPercent < .79) {
          $('.dot').removeClass('dotSelected');
 
-         // $('.aboutDot').removeClass('dotSelected');
-         // $('.projectsDot').removeClass('dotSelected');
          $('.section4Dot').addClass('dotSelected');
-         // $('.heroDot').removeClass('dotSelected');
       }
       else if (scrollPercent > .79 && scrollPercent < .87) {
          $('.dot').removeClass('dotSelected');
 
-         // $('.aboutDot').removeClass('dotSelected');
-         // $('.projectsDot').removeClass('dotSelected');
          $('.contactDot').addClass('dotSelected');
-         // $('.heroDot').removeClass('dotSelected');
       }
-      else if($(window).scrollTop() / baseHeight){
+      else if($(window).scrollTop() / initialHeight){
 
       }
       else {
@@ -73,26 +53,27 @@ app.navScroller = function () {
 }
 
 
-app.throttling = function (callback, limit, time) {
-   /// monitor the count
-   let calledCount = 0;
+// COME BACK TO THIS FUNCTION to Throttle my screen resizing joke.
+// I want to do something here that makes the screen show a funny thing when someone is RESIZING window width.
+// ------------------------------___--------------------------------------->
+// app.throttling = function (callback, limit, time) {
+//    /// monitor the count
+//    let calledCount = 0;
 
-   /// refesh the `calledCount` varialbe after the `time` has been passed
-   setInterval(function () { calledCount = 0 }, time);
+//    /// refesh the `calledCount` varialbe after the `time` has been passed
+//    setInterval(function () { calledCount = 0 }, time);
 
-   /// creating a clousre that will be called
-   return function () {
-      /// checking the limit (if limit is exceeded then do not call the passed function
-      if (limit > calledCount) {
-         /// increase the count
-         calledCount++;
-         callback(); /// call the function
-      }
-      else console.log('not calling because the limit has exeeded');
-   };
-}
-
-
+//    /// creating a clousre that will be called
+//    return function () {
+//       /// checking the limit (if limit is exceeded then do not call the passed function
+//       if (limit > calledCount) {
+//          /// increase the count
+//          calledCount++;
+//          callback(); /// call the function
+//       }
+//       else console.log('not calling because the limit has exeeded');
+//    };
+// }
 
 
 // app.windowWidthScanner = () => {
@@ -117,17 +98,12 @@ app.throttling = function (callback, limit, time) {
 //    })
 // }
 
-
-
-
-
-
+// ------------------------------___----------------------------------->
 
 app.init = function () {
    app.navScroller()
    app.windowWidthScanner()
 }
-
 
 
 $(function () {
